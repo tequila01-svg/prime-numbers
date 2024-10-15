6d9fee04d745709f65209136c7295ad64a686700
tequila01-svg <onifadeayobami008@gmail.com>
2024-10-15 18:19:27 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+num=int(input("enter a number"))
+if num>1:
+    for i in range (2, int(num**0.5)+1):
+        if num % i==0:
+         print(num,"is not a prime number")
+         break
+    else:
+         print(num,"is a prime number")
